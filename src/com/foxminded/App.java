063c878299b82545b25fa2fde8f8954c7527559d
Nikita Molodkin <molodkin1991@mail.ru>
2021-10-08 18:43:32 +0300

    initial commit

---
+++ src/com/foxminded/App.java
@@ -0,0 +1,8 @@
+package com.foxminded;
+
+public class App {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/foxminded/ReverseString.java
@@ -0,0 +1,4 @@
+package com.foxminded;
+
+public class ReverseString {
+}
