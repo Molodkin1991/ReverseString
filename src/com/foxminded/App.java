6c5764896ee8c201936b272a1909a1f1d82fe3fe
Nikita Molodkin <molodkin1991@mail.ru>
2021-10-08 18:47:23 +0300

    add class

--- src/com/foxminded/App.java
+++ src/com/foxminded/App.java
@@ -1,8 +1,21 @@
 package com.foxminded;
 
+import java.util.Scanner;
+
 public class App {
+    private static final String MESSAGE_FOR_INPUTTEXT = "Write some text:";
+    private static final String OUTPUT_MESSAGE = "Reversed text: ";
 
     public static void main(String[] args) {
-	// write your code here
+        ReverseString anagram = new ReverseString();
+        String text = readText();
+        String reversedText = anagram.reverseText(text);
+        System.out.println(OUTPUT_MESSAGE + reversedText);
+    }
+
+    public static String readText() {
+        Scanner in = new Scanner(System.in);
+        System.out.println(MESSAGE_FOR_INPUTTEXT);
+        return in.nextLine();
     }
 }
--- src/com/foxminded/ReverseString.java
+++ src/com/foxminded/ReverseString.java
@@ -1,4 +1,39 @@
 package com.foxminded;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class ReverseString {
+    private static final String DELIMITER = " ";
+
+    public String reverseText(String text) {
+        String[] words = text.split(DELIMITER);
+        return Arrays.stream(words).map(this::reverseWord).collect(Collectors.joining(DELIMITER));
+    }
+
+    private String reverseWord(String word) {
+        char[] wordCharacters = word.toCharArray();
+        int i = 0;
+        int j = wordCharacters.length-1;
+        while (i < j ) {
+            if (!Character.isAlphabetic(wordCharacters[i]))
+                i++;
+            else if (!Character.isAlphabetic(wordCharacters[j]))
+                j--;
+            else {
+                swapChars(wordCharacters, i, j);
+                i++;
+                j--;
+            }
+        }
+        return String.valueOf(wordCharacters);
+
+    }
+
+    private void swapChars(char[] wordCharacters, int i, int j) {
+        char current = wordCharacters[i];
+        wordCharacters[i] = wordCharacters[j];
+        wordCharacters[j] = current;
+    }
+
 }
